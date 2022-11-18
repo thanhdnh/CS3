c5e340781b9dc894a057e689b9e9ad4ffe6bf20e
thanhdnh <myhoangthanh@yahoo.com>
2022-11-18 14:21:43 +0700

    OKKKK

--- Program.cs
+++ Program.cs
@@ -9,13 +9,28 @@ public class Program
     Account acc1 = new Account("ID01", "abc", "123", "Hi!");
     Account acc2 = new Account("ID02", "def", "123", "Hello!");
     Account acc3 = new Account("ID03", "ikl", "123", "Hey!");
-    AccountDict accdict = new AccountDict();
+
+    /*AccountDict accdict = new AccountDict();
     accdict.Add(acc1.getId(), acc1);
     accdict.Add(acc2.getId(), acc2);
     accdict.Add(acc3.getId(), acc3);
     IDictionaryEnumerator en = accdict.GetEnumerator();
     while(en.MoveNext())
-      System.Console.WriteLine("{0}=>{1}", en.Key, en.Value);
+      System.Console.WriteLine("{0}=>{1}", en.Key, en.Value);*/
+
+    /*Dictionary<string, Account> dict = new Dictionary<string, Account>();
+    dict.Add(acc1.getId(), acc1);
+    dict.Add(acc2.getId(), acc2);
+    dict.Add(acc3.getId(), acc3);
+    foreach(KeyValuePair<string, Account> kvp in dict)
+      System.Console.WriteLine("{0}=>{1}", kvp.Key, kvp.Value);*/
+
+    SortedDictionary<string, Account> sdict = new SortedDictionary<string, Account>();
+    sdict.Add(acc2.getId(), acc2);
+    sdict.Add(acc3.getId(), acc3);
+    sdict.Add(acc1.getId(), acc1);
+    foreach(KeyValuePair<string, Account> kvp in sdict)
+      System.Console.WriteLine("{0}=>{1}", kvp.Key, kvp.Value);
 
     Console.ReadLine();
   }
