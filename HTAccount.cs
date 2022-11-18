6697a0a757690bcd99b16e748d9f452ad4ca5c8b
thanhdnh <myhoangthanh@yahoo.com>
2022-11-18 14:41:37 +0700

    OKKKK

--- HTAccount.cs
+++ HTAccount.cs
@@ -1,12 +1,14 @@
 using System.Collections;
-class HTAccount{
-    Hashtable infos = new Hashtable();
-    public HTAccount(string id, string username, string password, string status){
-        infos["id"] = id;
-        infos["username"] = username;
-        infos.Add("password", password);
-        infos.Add("status", status);
-    }
+class HTAccount
+{
+  Hashtable infos = new Hashtable();
+  public HTAccount(string id, string username, string password, string status)
+  {
+    infos["id"] = id;
+    infos["username"] = username;
+    infos.Add("password", password);
+    infos.Add("status", status);
+  }
   public override string ToString()
   {
     return $"Account[id: {infos["id"]}, username: {infos["username"]}, password: {infos["password"]}, status: {infos["status"]}]";
